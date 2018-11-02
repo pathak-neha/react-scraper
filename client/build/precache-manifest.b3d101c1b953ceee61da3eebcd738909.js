43601327e954d89451d91ff42da0796847cef68c
Neha Pathak <neha.pathak@hotmail.com>
2018-11-02 05:36:14 -0400

    ran build on app

---
+++ client/build/precache-manifest.b3d101c1b953ceee61da3eebcd738909.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4db5235dbfc4c4523008",
+    "url": "/static/js/main.4db5235d.chunk.js"
+  },
+  {
+    "revision": "d42dac8844db0cfa72c6",
+    "url": "/static/js/1.d42dac88.chunk.js"
+  },
+  {
+    "revision": "4db5235dbfc4c4523008",
+    "url": "/static/css/main.b774ac37.chunk.css"
+  },
+  {
+    "revision": "2448d533cff47f0fb8036961450b8180",
+    "url": "/index.html"
+  }
+];
